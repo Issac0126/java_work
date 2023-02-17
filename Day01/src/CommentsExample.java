33984ee7782584419ebcb3cfc0eda457e1d4fa7d
leeyoonseo <diano679@gmail.com>
2023-02-17 18:18:58 +0900

    txt 지우고 다시 리밋 진짜 최종

---
+++ Day01/src/CommentsExample.java
@@ -0,0 +1,30 @@
+/** 저작권자나, 해당 파일이 어떤 것인지 적는 주석
+ *  대게 배포용 문서에 설명을 달아 놓을 때 사용합니다.
+ *  @author (저작권자) 나!
+ *  @version 1.0
+ *  @since (일자) 2023.02.17 ~ */
+//이 파일은 주석을 연습하는 파일입니답
+public class CommentsExample {
+
+	public static void main(String[] args) {
+//		어디서든지 주석 사용 가능. 단, 한 줄만 주석으로 기능함.
+		
+		/*
+		# main 함수(메서드)는 프로그램 실행 시 자동으로 동작하는 함수이며, 
+		반드시 필요한 함수입니다.
+		
+		# 자바 코드를 해석하여 실행시키는 자바 가상 머신(JVM)은 
+		프로그램 실행 시 자동으로 main 함수를 불러서 내부의 코드를
+		실행시킵니다.
+		*/
+		
+//		원하는 위치에 커서 배치 후, Ctrl + / : 빠른 추석 처리, 해제
+		
+		System.out.println("sicout 입력 후 컨트롤 스페이스바로 자동완성");
+		System.out.println("siso도 가능함.");
+		System.out.println("siso도 가능함.");
+		System.out.println("siso도 가능함.");
+		//syso 입력 + Ctrl + space : 빠른 출력 함수를 생성
+	}
+
+}
---
+++ Day01/src/Identifier.java
@@ -0,0 +1,57 @@
+
+public class Identifier {
+
+	public static void main(String[] args) {
+		
+		// 꼭 지켜야 할 규칙 (안 지킬 시 에러가 발생할 수 있습니다.)
+		// 1. 식별자의 이름은 중복을 허용하지 않는다. 
+		// 대/소문자를 철저하게*** 구분함.
+		
+		int age = 35;
+//		int age = 40; (x) _ age로 이름이 중복됨.
+		int Age = 40;
+		// age와 Age로 이름이 중복되지 않음.
+		
+		System.out.println(age);
+		System.out.println(Age);
+		
+		
+		// 2. 식별자 이름은 숫자로 지정하거나 숫자로 시작하면 안된다.
+//		int 7hello = 365; (x)
+		int hello700 = 7;
+		int ho7ho = 50;
+		
+		
+		// 3. 식별자 이름에 공백을 포함 불가.
+//		int my birth day = 0126 (x)
+		int myBirthDay = 0126;
+//		int my_birth_day = 0126; _가능은 하나... 특수문자는 권장하지 않음.
+		// 띄어쓰기를 못하는 대신, 연결자의 첫글자만 대문자로 표기하는 암묵적 룰이 있음. 
+//		    =camel case 표기법. ^ㅁ^ 
+		
+		
+		// 4.식별자 이름에 쓸 수 있는 특수문자는 언더바(_), 달러기호($)만 가능.
+		// 단, 사용을 권장하지는 않는데, 해당 특수문자들은 사용처가 따로 있기 때문.
+		String $hello_ = "안녕";
+		
+		
+		// 5. 키워드(예약어)는 식별자 이름으로 사용 불가능.
+		// 키워드는 이미 어떠한 기능을 내포하고 있는 지정 문자이기 때문.
+//		int int = 21; (x)
+//		String class = "클래스"; (x)
+//		String Class = "클래스"; (권장X) 가능은 하나... 헷갈리니 권장X.
+		String classname = "클래스";
+		
+		
+		// 6. 한글이나 한자 같은 식별자 이름도 지정이 가능하다.
+		// 하지만 권장하지는 않는다... 가독성이 떨어지고, UTF-8이 아닌 환경으로 들어가면 한글이 깨짐.
+		int 숫자 = 10; 
+		String 名 = "홍길동";
+		System.out.println(숫자);
+		System.out.println(名);
+		
+		
+		
+	}
+
+}
